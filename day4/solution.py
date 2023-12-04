514b8bf9b8825fa6ded6c06f2ebd45991757cbeb
orhun23 <orhun2303@gmail.com>
2023-12-04 20:08:40 +0100

    idk why this dont work

---
+++ day4/solution.py
@@ -0,0 +1,11 @@
+t = 0
+
+for x in open("C:\\Users\\orhun\\OneDrive\\Documenten\\adventofcode\\Advent2023-1\\day4\\input.txt"):
+    
+    x = x.split(":")[1].strip()
+    a, b = [list(map(int, k.split())) for k in x.split(" | ")]
+    j = sum(q in a for q in b)
+    if j > 0:
+        t = t + 2 ** (j - 1)
+
+print(t)
