73169c846c597214856d3c596c6b8abb9e74d6c3
orhun23 <orhun2303@gmail.com>
2023-12-09 21:25:30 +0100

    ewaja

--- day8/solutionp1.py
+++ day8/solutionp1.py
@@ -1,2 +1,18 @@
-t = 0
 
+steps, _, *rest = open("C:\\Users\\orhun\\OneDrive\\Documenten\\adventofcode\\Advent2023-1\\day8\\input.txt").read().splitlines()
+
+network = {}
+
+for line in rest:
+    pos, targets = line.split(" = ")
+    network[pos] = targets[1:-1].split(", ")
+
+step_count = 0
+current = "AAA"
+
+while current != "ZZZ":
+    step_count += 1
+    current = network[current][0 if steps[0] == "L" else 1]
+    steps = steps[1:] + steps[0]
+
+print(step_count)
